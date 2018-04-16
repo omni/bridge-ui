e4a08d48c479a0844a29f72cfe06c32e7ce5efc5
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2018-04-16 14:25:24 -0300

    Fix a11y warnings

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,13 @@ export const Footer = () => (
   <footer className="footer">
     <div className="container">
       <p className="footer-rights">2018 POA Network. All rights reserved.</p>
-      <a href="#"> <i className="footer-logo" /></a>
+      <a href="#"><i className="footer-logo" /></a>
       <div className="socials">
-        <a href="https://www.reddit.com/r/POA/" target="_blank" className="socials-i socials-i_reddit" rel="noopener noreferrer"></a>
-        <a href="https://twitter.com/poanetwork" target="_blank" className="socials-i socials-i_twitter" rel="noopener noreferrer"></a>
-        <a href="https://poa.net" target="_blank" className="socials-i socials-i_oracles" rel="noopener noreferrer"></a>
-        <a href="https://t.me/poa_network" target="_blank" className="socials-i socials-i_telegram" rel="noopener noreferrer"></a>
-        <a href="https://github.com/poanetwork/" target="_blank" className="socials-i socials-i_github" rel="noopener noreferrer"></a>
+        <a href="https://www.reddit.com/r/POA/" target="_blank" className="socials-item" rel="noopener noreferrer"><i className="socials-i socials-i_reddit" /></a>
+        <a href="https://twitter.com/poanetwork" target="_blank" className="socials-item" rel="noopener noreferrer"><i className="socials-i socials-i_twitter" /></a>
+        <a href="https://poa.net" target="_blank" className="socials-item" rel="noopener noreferrer"><i className="socials-i socials-i_oracles" /></a>
+        <a href="https://t.me/poa_network" target="_blank" className="socials-item" rel="noopener noreferrer"><i className="socials-i socials-i_telegram" /></a>
+        <a href="https://github.com/poanetwork/" target="_blank" className="socials-item" rel="noopener noreferrer"><i className="socials-i socials-i_github" /></a>
       </div>
     </div>
   </footer>
