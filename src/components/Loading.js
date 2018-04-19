f62f726bede7cbf91155de2dc8bda06c3394406d
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2018-04-19 08:58:32 -0300

    Loading refactor

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,21 +4,18 @@ import { inject, observer } from "mobx-react";
 @inject("RootStore")
 @observer
 export class Loading extends React.Component {
-  constructor(props){
-    super(props)
-    this.alertStore = props.RootStore.alertStore;
-  }
   render() {
-    const style = this.alertStore.showLoading ? {display: 'block'} : {display: 'none'}
+    const { alertStore } = this.props.RootStore
+    const style = alertStore.showLoading ? {display: 'block'} : {display: 'none'}
     return (
       <div className="loading-container" style={style}>
         <div className="loading">
-          <div className="loading-i"></div>
-          <div className="loading-i"></div>
-          <div className="loading-i"></div>
-          <div className="loading-i"></div>
-          <div className="loading-i"></div>
-          <div className="loading-i"></div>
+          <div className="loading-i" />
+          <div className="loading-i" />
+          <div className="loading-i" />
+          <div className="loading-i" />
+          <div className="loading-i" />
+          <div className="loading-i" />
         </div>
       </div>
     )
