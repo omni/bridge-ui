ae346c83141e2fbfedf24dde08e85b8dd0540da5
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2018-04-23 15:14:35 -0300

    update Footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,10 @@ import React from 'react';
 export const Footer = () => (
   <footer className="footer">
     <div className="container">
-      <p className="footer-rights">2018 POA Network. All rights reserved.</p>
+      <p className="footer-rights">2018 POA. All rights reserved.</p>
       <a href="#"><i className="footer-logo" /></a>
       <div className="socials">
-        <a href="https://www.reddit.com/r/POA/" target="_blank" className="socials-item" rel="noopener noreferrer"><i className="socials-i socials-i_reddit" /></a>
         <a href="https://twitter.com/poanetwork" target="_blank" className="socials-item" rel="noopener noreferrer"><i className="socials-i socials-i_twitter" /></a>
-        <a href="https://poa.net" target="_blank" className="socials-item" rel="noopener noreferrer"><i className="socials-i socials-i_oracles" /></a>
         <a href="https://t.me/poa_network" target="_blank" className="socials-item" rel="noopener noreferrer"><i className="socials-i socials-i_telegram" /></a>
         <a href="https://github.com/poanetwork/" target="_blank" className="socials-item" rel="noopener noreferrer"><i className="socials-i socials-i_github" /></a>
       </div>
