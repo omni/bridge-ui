0aabcb4a7a1768f38eff78dfca7c5f2dff912e92
dennis00010011b <dennis00010011b@gmail.com>
2018-04-28 19:16:33 -0700

    identations Utils.js

--- e2e-script/Utils.js
+++ e2e-script/Utils.js
@@ -7,39 +7,39 @@ class Utils {
 
   static  async getHomeAccount() {
     try {
-	  let obj = JSON.parse(fs.readFileSync(configFile), "utf8");
-	  return obj.homeAccount;
-	} catch (err) {
-	  return null;
-	}
-  }
+      let obj = JSON.parse(fs.readFileSync(configFile), "utf8");
+      return obj.homeAccount;
+    } catch (err) {
+      return null;
+    }
+ }
 
   static async getForeignAccount() {
     try {
-	  let obj = JSON.parse(fs.readFileSync(configFile), "utf8");
-	  return obj.foreignAccount;
-	} catch (err) {
-	    return null;
+      let obj = JSON.parse(fs.readFileSync(configFile), "utf8");
+      return obj.foreignAccount;
+    } catch (err) {
+      return null;
     }
   }
 
   static async getStartURL() {
     try {
-	  let obj = JSON.parse(fs.readFileSync(configFile), "utf8");
-	  return obj.startUrl;
-	} catch (err) {
-	  return null;
-	}
+      let obj = JSON.parse(fs.readFileSync(configFile), "utf8");
+      return obj.startUrl;
+    } catch (err) {
+      return null;
+    }
   }
 
   static async  startBrowserWithMetamask() {
     let source = './e2e-script/MetaMask.crx';
-	let options = new chrome.Options();
-	await options.addExtensions(source);
-	await options.addArguments('disable-popup-blocking');
-	let driver = await new webdriver.Builder().withCapabilities(options.toCapabilities()).build();
+    let options = new chrome.Options();
+    await options.addExtensions(source);
+    await options.addArguments('disable-popup-blocking');
+    let driver = await new webdriver.Builder().withCapabilities(options.toCapabilities()).build();
     await driver.sleep(5000);
-	return driver;
+    return driver;
   }
 
 }
