be60ff1f14ab1b6b07ed7a2d98732f70c6f20273
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2018-05-03 15:07:47 -0300

    Update loading component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import { inject, observer } from "mobx-react";
+import loadingLogo from '../assets/images/logos/logo-loader.svg'
 
 @inject("RootStore")
 @observer
 export class Loading extends React.Component {
   render() {
     const { alertStore } = this.props.RootStore
-    const style = alertStore.showLoading ? {display: 'block'} : {display: 'none'}
+    const style = alertStore.showLoading ? {display: 'flex'} : {display: 'none'}
     return (
       <div className="loading-container" style={style}>
-        <div className="loading">
-          <div className="loading-i" />
-          <div className="loading-i" />
-          <div className="loading-i" />
-          <div className="loading-i" />
-          <div className="loading-i" />
-          <div className="loading-i" />
-        </div>
+        <img className="loading" src={loadingLogo} alt="loading"/>
+        <div className="loading-i" />
       </div>
     )
   }
