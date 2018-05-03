8da738a2562fd8ab9d7557509b1b699614a9638d
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2018-05-03 14:26:39 -0300

    Add disclaimer

--- src/App.js
+++ src/App.js
@@ -2,17 +2,46 @@ import React from 'react';
 import { Header, Bridge, RelayEvents, Footer, SweetAlert, Loading } from './components';
 import { Route } from 'react-router-dom'
 import './assets/stylesheets/application.css';
-export const App = () => {
-  return (
-    <div>
-      <Route component={Loading}/>
-      <Route component={SweetAlert}/>
-      <Route component={Header}/>
-      <div className="app-container">
-        <Route exact path="/" component={Bridge}/>
-        <Route exact path="/events" component={RelayEvents}/>
+import { Disclaimer } from './components'
+import { ModalContainer } from './components'
+import { setItem, getItem, DISCLAIMER_KEY } from './components/utils/localstorage'
+
+export class App extends React.Component {
+  state = {
+    showDisclaimer: false
+  }
+
+  componentDidMount() {
+    const disclaimerDisplayed = getItem(DISCLAIMER_KEY)
+    if(!disclaimerDisplayed) {
+      this.setState({ showDisclaimer: true })
+    }
+  }
+
+  closeDisclaimer = () => {
+    setItem(DISCLAIMER_KEY, true)
+    this.setState({showDisclaimer: false})
+  }
+
+  render() {
+    const { showDisclaimer } = this.state
+    return (
+      <div>
+        <Route component={Loading}/>
+        <Route component={SweetAlert}/>
+        <Route component={Header}/>
+        <div className="app-container">
+          <Route exact path="/" component={Bridge}/>
+          <Route exact path="/events" component={RelayEvents}/>
+        </div>
+        <Route component={Footer}/>
+        <ModalContainer
+          showModal={showDisclaimer}
+        >
+          <Disclaimer
+            onConfirmation={this.closeDisclaimer} />
+        </ModalContainer>
       </div>
-      <Route component={Footer}/>
-    </div>
-  );
-};
+    )
+  }
+}
