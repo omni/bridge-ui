eb84d48ab75430a91b1b7a318e1b224aa8975578
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2018-05-10 14:03:03 -0300

    add commas on network balance

--- src/components/BridgeNetwork.js
+++ src/components/BridgeNetwork.js
@@ -11,7 +11,7 @@ export const BridgeNetwork = ({
   showModal
 }) => {
   const containerName = isHome ? 'home' : 'foreign'
-  const formattedBalance = numeral(balance).format('0.00', Math.floor)
+  const formattedBalance = numeral(balance).format('0,0.00', Math.floor)
 
   const showMore = () => isHome ?
     (<div className="bridge-network-data" onClick={showModal}>
