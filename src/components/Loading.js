c1b38f63a40b153e1515500b7b26fb12f8a117be
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2018-05-10 14:58:48 -0300

    fix loading on mobile

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -12,14 +12,14 @@ export class Loading extends React.Component {
     const style = alertStore.showLoading ? {display: 'flex'} : {display: 'none'}
     const progress = loadingStepIndex === 3 ? 100 : (loadingStepIndex) * 25 + blockConfirmations * 4
     return (
-      <div className="loading-container" style={style}>
+      <div className={`loading-container ${loadingStepIndex > 0 ? 'mobile-container' : ''}`} style={style}>
         {loadingStepIndex > 0 && <ProgressRing
           radius={ 40 }
           stroke={ 4 }
           progress={progress}
           confirmationNumber={blockConfirmations}
         />}
-        {loadingStepIndex === 0 && <img className="loading" src={loadingLogo} alt="loading"/>}
+        {loadingStepIndex === 0 && (<img className="loading" src={loadingLogo} alt="loading"/>)}
         {loadingStepIndex === 0 && <div className="loading-i" />}
         {loadingStepIndex > 0 && (<div className="loading-text">{loadingSteps[loadingStepIndex]}</div>)}
       </div>
