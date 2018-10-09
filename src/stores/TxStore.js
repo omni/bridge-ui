ff6b80742727007ea34058a18574ee9c6a09e739
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2018-10-09 12:02:03 -0300

    Update success status response check

--- src/stores/TxStore.js
+++ src/stores/TxStore.js
@@ -97,9 +97,10 @@ class TxStore {
 
   async getTxStatus(hash) {
     const web3 = this.web3Store.injectedWeb3;
+    const { toBN } = web3.utils
     web3.eth.getTransactionReceipt(hash, (error, res) => {
       if(res && res.blockNumber){
-        if(res.status === '0x1'){
+        if(toBN(res.status).eq(toBN(1))){
           if(this.web3Store.metamaskNet.id === this.web3Store.homeNet.id.toString()) {
             const blockConfirmations = this.homeStore.latestBlockNumber - res.blockNumber
             if(blockConfirmations >= 8) {
