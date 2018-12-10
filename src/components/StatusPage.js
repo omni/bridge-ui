03c3bc055dc882fc336b6c2e19faef17091acabd
fernandomg <fernando.greco@gmail.com>
2018-12-10 11:43:08 -0300

    Prevent from displaying 'Authorities' in `status` page, if the network doesn't support events

--- src/components/StatusPage.js
+++ src/components/StatusPage.js
@@ -3,6 +3,7 @@ import { inject, observer } from "mobx-react"
 import { Configuration } from './Configuration'
 import { Authority } from './Authority'
 import pattern from '../assets/images/pattern.svg'
+import yn from 'yn'
 
 
 @inject("RootStore")
@@ -18,6 +19,9 @@ export class StatusPage extends React.Component {
     const maxSingleDeposit = isHome ? homeStore.maxPerTx : foreignStore.maxPerTx
     const maxTotalBalance = isHome ? homeStore.maxCurrentDeposit : foreignStore.maxCurrentDeposit
     const validatorsList = isHome ? homeStore.validators : foreignStore.validators
+    const { REACT_APP_HOME_WITHOUT_EVENTS: HOME, REACT_APP_FOREIGN_WITHOUT_EVENTS: FOREIGN } = process.env
+    const withoutEvents = web3Store.metamaskNet.id === web3Store.homeNet.id.toString() ? yn(HOME) : yn(FOREIGN)
+
     return (
       <div className="status-page">
         <div className='status-left-container' />
@@ -31,14 +35,16 @@ export class StatusPage extends React.Component {
                 maxSingleDeposit={maxSingleDeposit}
                 maxTotalBalance={maxTotalBalance} />
           </div>
-          <div className='status-authorities-container'>
-            <span className='status-authorities-title status-title'>Authorities</span>
-            <div className='status-authorities-data'>
-              {validatorsList.map((validator,i) => (
-                <Authority key={validator} address={validator} number={(i+1)} logoIndex={(i) % 3} />
-              ))}
+          {withoutEvents ? null :
+            <div className='status-authorities-container'>
+              <span className='status-authorities-title status-title'>Authorities</span>
+              <div className='status-authorities-data'>
+                {validatorsList.map((validator,i) => (
+                  <Authority key={validator} address={validator} number={(i+1)} logoIndex={(i) % 3} />
+                ))}
+              </div>
             </div>
-          </div>
+          }
         </div>
         <div className='status-right-container'>
           <img className='status-right-image' src={pattern} alt=""/>
