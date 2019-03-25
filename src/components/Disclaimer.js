37f613de155dbbf0dbfe00f97a24627d41a66c4b
Siddig Zeidan <siddigie1234@gmail.com>
2019-03-25 17:48:19 -0400

    feat(text): Updated the text and links for some components

--- src/components/Disclaimer.js
+++ src/components/Disclaimer.js
@@ -7,22 +7,22 @@ export const Disclaimer = ({ onConfirmation }) => (
       <img className="disclaimer-icon" src={disclaimerIcon} alt="disclaimer icon"/>
     </div>
     <div className="alert-container">
-      <span className="disclaimer-title">Welcome to <br/> POA Bridge UI App Beta+</span>
+      <span className="disclaimer-title">Welcome to the<br/> TokenBridge UI App Beta+</span>
       <p className="disclaimer-description">
-        We’re launching our POA Bridge and our UI App on a beta-testing basis. While we’ve
+        We’re launching our TokenBridge and our UI App on a beta-testing basis. While we’ve
         worked long and hard to develop the core features of the software, we expect that our
-        users may detect bugs and other issues. Help us improve by submitting tickets to our
-        <a href="https://poanet.zendesk.com/hc/en-us/categories/360000349273-POA-Bridge" target="_blank"
-           rel="noopener noreferrer"> support page</a>.
+        users may detect bugs and other issues. Help us improve by posting any difficulties to our
+        <a href="https://forum.poa.network/c/support/tokenbridge-support" target="_blank"
+          rel="noopener noreferrer"> support page</a>.
         <br/>
         <br/>
-        Use of this app and the POA Bridge is at your own risk. Users may experience
+        Use of this app and the TokenBridge is at your own risk. Users may experience
         unexpected delays, unexpected visual artifacts, unexpected loss of tokens or funds from
         improper app configuration, or other negative outcomes.
         <br/>
         <br/>
         By hitting the "continue" button, you are representing that you’ve read our <a
-        href="https://poanet.zendesk.com/hc/en-us/articles/360004054794-Terms-and-Conditions-of-The-POA-Bridge"
+          href="hhttps://forum.poa.network/t/end-user-licensing-agreement-and-terms-of-service/2197"
         target="_blank" rel="noopener noreferrer">Terms of
         Service</a> in full, and that you agree to be legally bound by them.
       </p>
