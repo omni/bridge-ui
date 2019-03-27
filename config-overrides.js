58f220ba5d9dc2e451fc56bedc1c8c8cd9464661
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2019-03-27 10:33:43 -0300

    Update react build dependencies

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,10 @@
-const rewireMobX = require('react-app-rewire-mobx');
+const {
+  addDecoratorsLegacy,
+  disableEsLint,
+  override
+} = require("customize-cra");
 
-/* config-overrides.js */
-module.exports = function override(config, env) {
-  // use the MobX rewire
-  config = rewireMobX(config,env);
-  
-  return config;
-}
+module.exports = override(
+    addDecoratorsLegacy(),
+    disableEsLint()
+  );
