88d2473427c9f7644eec49f665254d13a54281b2
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2019-03-28 15:15:27 -0300

    Changes for own web3 library

--- src/stores/TxStore.js
+++ src/stores/TxStore.js
@@ -40,13 +40,9 @@ class TxStore {
           addPendingTransaction()
           this.getTxReceipt(hash)
         }).on('error', (e) => {
-          if(e.message.includes('User denied transaction signature')) {
+          if(!e.message.includes('not mined within 50 blocks') && !e.message.includes('Failed to subscribe to new newBlockHeaders')) {
             this.alertStore.setLoading(false)
             this.alertStore.pushError('Transaction rejected on wallet');
-          } else if(!e.message.includes('not mined within 50 blocks') && !e.message.includes('Failed to subscribe to new newBlockHeaders')){
-            // False error is thrown by web3 https://github.com/ethereum/web3.js/issues/2542
-            // this.alertStore.setLoading(false)
-            // this.alertStore.pushError('Transaction rejected on wallet');
           }
         })
       } catch(e) {
