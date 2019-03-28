fe27eba69b51afe173103dd07c70ebdabcd047ee
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2019-03-28 17:18:47 -0300

    Replace own web3 for 1.0.0-beta.30 and add modified web3-eth

--- src/stores/TxStore.js
+++ src/stores/TxStore.js
@@ -41,11 +41,9 @@ class TxStore {
           addPendingTransaction()
           this.getTxReceipt(hash)
         }).on('error', (e) => {
-          if(e.message.includes('User denied transaction signature')) {
+          if(!e.message.includes('not mined within 50 blocks') && !e.message.includes('Failed to subscribe to new newBlockHeaders')){
             this.alertStore.setLoading(false)
             this.alertStore.pushError('Transaction rejected on wallet');
-          } else if(!e.message.includes('not mined within 50 blocks') && !e.message.includes('Failed to subscribe to new newBlockHeaders')){
-            console.log(e)
           }
         })
       } catch(e) {
