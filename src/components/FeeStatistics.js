f1be6e1214991306f9538b977e43aefcfb9cab09
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2019-04-08 14:59:05 -0300

    Add FeeStatistics unit test

--- src/components/FeeStatistics.js
+++ src/components/FeeStatistics.js
@@ -3,13 +3,14 @@ import numeral from 'numeral'
 import { DataBlock } from './DataBlock'
 
 export const FeeStatistics = ({ depositFeeCollected, withdrawFeeCollected }) => (
-  <div className="statistics-fee-data">
+  <div className="statistics-fee-data" data-testid="fee-statistics">
     {
       depositFeeCollected.shouldDisplay &&
       <DataBlock
         description="Deposit Fees"
         value={numeral(depositFeeCollected.value).format('0,0.00 a', Math.floor)}
         type={depositFeeCollected.type}
+        dataTestid="deposit-fees-block"
       />
     }
     {
@@ -23,6 +24,7 @@ export const FeeStatistics = ({ depositFeeCollected, withdrawFeeCollected }) => 
         description="Withdrawal Fees"
         value={numeral(withdrawFeeCollected.value).format('0,0.00 a', Math.floor)}
         type={withdrawFeeCollected.type}
+        dataTestid="withdrawal-fees-block"
       />
     }
   </div>
