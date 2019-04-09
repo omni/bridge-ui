6616bd74773a3b5cad34500cf33acdd33eae53af
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2019-04-09 16:19:40 -0300

    Refactor getValidatorList

--- src/stores/utils/contract.js
+++ src/stores/utils/contract.js
@@ -2,9 +2,7 @@ import BN from 'bignumber.js'
 import { fromDecimals } from './decimals'
 import { fromWei } from 'web3-utils'
 import { FEE_MANAGER_MODE } from './bridgeMode'
-import BridgeValidatorsV1Abi from './BridgeValidatorsV1.abi'
 import { abi as BRIDGE_VALIDATORS_ABI } from '../../contracts/BridgeValidators'
-import { abi as REWARDABLE_BRIDGE_VALIDATORS_ABI } from '../../contracts/RewardableValidators'
 
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
@@ -70,40 +68,48 @@ export const totalBurntCoins = async (contract) => {
 }
 
 export const getValidatorList = async (address, eth) => {
-  const v1Contract = new eth.Contract(BridgeValidatorsV1Abi, address);
-  const v1ValidatorsEvents = await getValidatorEvents(v1Contract)
+  const validatorsContract = new eth.Contract(BRIDGE_VALIDATORS_ABI, address);
 
-  const contract = new eth.Contract(BRIDGE_VALIDATORS_ABI, address);
-  const deployedAtBlock = await getDeployedAtBlock(contract);
-  const validatorsEvents = await getValidatorEvents(contract, deployedAtBlock)
+  const deployedAtBlock = await getDeployedAtBlock(validatorsContract);
+  const contract = new eth.Contract([], address)
+  const validatorsEvents = await contract.getPastEvents('allEvents', { fromBlock: Number(deployedAtBlock) })
 
-  const rewardableContract = new eth.Contract(REWARDABLE_BRIDGE_VALIDATORS_ABI, address);
-  const rewardableValidatorsEvents = await getValidatorEvents(rewardableContract, deployedAtBlock)
-
-  const eventList = [...v1ValidatorsEvents, ...validatorsEvents, ...rewardableValidatorsEvents]
-
-  const sortedEvents = eventList.sort((a, b) => a.blockNumber - b.blockNumber)
-
-  return processValidatorsEvents(sortedEvents)
+  return processValidatorsEvents(validatorsEvents)
 }
 
-const processValidatorsEvents = (events) => {
+export const processValidatorsEvents = (events) => {
   const validatorList = new Set()
   events.forEach(event => {
+    parseValidatorEvent(event)
+
     if(event.event === 'ValidatorAdded') {
       validatorList.add(event.returnValues.validator)
     } else if(event.event === 'ValidatorRemoved') {
       validatorList.delete(event.returnValues.validator)
     }
     })
+
   return Array.from(validatorList)
 }
 
-const getValidatorEvents = async (bridgeValidatorContract, fromBlock = 0) => {
-  try {
-    return await bridgeValidatorContract.getPastEvents('allEvents', { fromBlock })
-  } catch (e) {
-    return []
+export const parseValidatorEvent = (event) => {
+  if (event.event === undefined
+    && event.raw
+    && event.raw.topics
+    && (event.raw.topics[0] === '0xe366c1c0452ed8eec96861e9e54141ebff23c9ec89fe27b996b45f5ec3884987'
+      || event.raw.topics[0] === '0x8064a302796c89446a96d63470b5b036212da26bd2debe5bec73e0170a9a5e83')) {
+    const rawAddress = event.raw.data === '0x' ? event.raw.topics[1] : event.raw.data
+    const address = '0x' + rawAddress.slice(26)
+    event.event = 'ValidatorAdded'
+    event.returnValues.validator = address
+  } else if (event.event === undefined
+    && event.raw
+    && event.raw.topics
+    && event.raw.topics[0] === "0xe1434e25d6611e0db941968fdc97811c982ac1602e951637d206f5fdda9dd8f1") {
+    const rawAddress = event.raw.data === '0x' ? event.raw.topics[1] : event.raw.data
+    const address = '0x' + rawAddress.slice(26)
+    event.event = 'ValidatorRemoved'
+    event.returnValues.validator = address
   }
 }
 
