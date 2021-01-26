016b6c12fa6d2502ad0132f4e19c159332d3b0e0
Kirill Fedoseev <k.fedoseev@innopolis.university>
2021-01-26 21:32:14 +0300

    Fix chain id comparison

--- src/stores/TxStore.js
+++ src/stores/TxStore.js
@@ -90,7 +90,7 @@ class TxStore {
         if(res.status === '0x1'){
           const index = this.txHashToIndex[hash]
           this.txs[index].status = `mined`
-          if(this.web3Store.metamaskNet.id === this.web3Store.homeNet.id.toString()) {
+          if(this.web3Store.metamaskNet.id.toString() === this.web3Store.homeNet.id.toString()) {
             const blockConfirmations = this.homeStore.latestBlockNumber - res.blockNumber
             if(blockConfirmations >= 8) {
               this.alertStore.setBlockConfirmations(8)
@@ -100,7 +100,9 @@ class TxStore {
               if(blockConfirmations > 0) {
                 this.alertStore.setBlockConfirmations(blockConfirmations)
               }
-              this.getTxStatus(hash)
+              setTimeout(() => {
+                this.getTxStatus(hash)
+              }, 5000)
             }
 
           } else {
@@ -113,7 +115,9 @@ class TxStore {
               if(blockConfirmations > 0) {
                 this.alertStore.setBlockConfirmations(blockConfirmations)
               }
-              this.getTxStatus(hash)
+              setTimeout(() => {
+                this.getTxStatus(hash)
+              }, 5000)
             }
           }
         } else {
@@ -124,7 +128,9 @@ class TxStore {
           this.alertStore.pushError(`${hash} Mined but with errors. Perhaps out of gas`)
         }
       } else {
-        this.getTxStatus(hash)
+        setTimeout(() => {
+          this.getTxStatus(hash)
+        }, 5000)
       }
     })
   }
